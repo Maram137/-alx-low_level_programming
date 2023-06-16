a65ebcd704e47e0f169eb43498fef5b051c92fea
maram matmati <matmatimaram137@gmail.com>
2023-06-15 18:20:55 -0700

    programme

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
- *  main - A program that prints a line with puts function
- *  retun: 0 (Success)
+ * main - A program that prints a line with puts function
+ * Return: 0 (success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
