4fc5ef34fdb9c39ad585c00f0ffc4c10ed92e2ca
maram matmati <matmatimaram137@gmail.com>
2023-06-21 17:40:29 -0700

    1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,42 @@
 #include "main.h"
 /**
+*print_times_table - Prints the times table of a given number
+*@n: The number to print the times table for
+*/
+void print_times_table(int n)
+{
+int i, j, product;
+if (n < 0 || n > 9)
+return;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+product = i * j;
+if (j == 0)
+_putchar('0' + product);
+else
+{
+_putchar(' ');
+_putchar(' ');												
+if (product < 10)											
+_putchar(' ');
+else if (product >= 10 && product < 100)										_putchar(' ');	
+printf("%d", product);													
+}						        
+if (j != n)
+_putchar(',')
+}
+_putchar('\n');
+}
+}
+/**
 *main - Entry point
-*Return: Always 0.
+*Return: Always 0
 */
 int main(void)
 {
-char str[] = "_putchar\n";
-int i = 0;
-while (str[i] != '\0')
-{
-_putchar(str[i]);
-i++;
-}
+ print_times_table(9);
 return (0);
 }
+
