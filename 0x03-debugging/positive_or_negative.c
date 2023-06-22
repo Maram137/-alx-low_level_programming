48813236de4b6e924f418a8a904c03dd80313e41
maram matmati <matmatimaram137@gmail.com>
2023-06-22 14:22:56 -0700

    c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -12,16 +12,3 @@ printf("%d is positive\n", i);
 else
 printf("%d is negative\n", i);
 }
-
-/**
-*main - tests function that prints if integer is positive or negative
-*Return: 0
-*/
-int main(void)
-{
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
-}
-
