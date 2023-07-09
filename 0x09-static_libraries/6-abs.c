0749e2e71b3e0f4c52833eb5ec0eeadf656fe7b8
maram matmati <matmatimaram137@gmail.com>
2023-07-08 17:44:12 -0700

    static

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_abs - print the absolute value of number given
+*@n: int type number
+*Return: n
+*/
+int _abs(int n)
+{
+if (n < 0)
+{
+return (n * -1);
+}
+return (n);
+}
